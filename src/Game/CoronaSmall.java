3d790b1a4794e45f5b97c072459116210a2c1081
Andrii Tkachenko <andrii.tkachenko@ukma.edu.ua>
2020-05-21 20:32:57 +0300

    No changes

--- src/Game/CoronaSmall.java
+++ src/Game/CoronaSmall.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Game;
 
 import org.newdawn.slick.SlickException;
@@ -76,83 +76,3 @@ public class CoronaSmall extends Rectangle implements Enemy {
 
 
 }
-||||||| merged common ancestors
-=======
-package Game;
-
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Rectangle;
-
-public class CoronaSmall extends Rectangle implements Enemy {
-
-    private boolean blockedLeft = false;
-    private boolean blockedRight = false;
-    private boolean blockedUp = false;
-    private boolean blockedDown = false;
-
-    private float babkaX;
-    private float babkaY;
-    private float speed;
-
-    private boolean alive = true;
-
-
-    public CoronaSmall (int x, int y) throws SlickException {
-        super(x, y, 25, 25);
-    }
-
-
-    public void update() {
-        move();
-        blockedLeft = false;
-        blockedRight = false;
-        blockedDown = false;
-        blockedLeft = false;
-    }
-
-
-    private void move() {
-                if (this.getX() < babkaX && blockedRight == false) {
-                    this.setCenterX(getCenterX() + speed);
-
-                } if (this.getX() > babkaX && blockedLeft == false) {
-                    this.setCenterX(getCenterX() - speed);
-                }
-             if (this.getY() > babkaY  && blockedUp == false) {
-                    this.setCenterY(getCenterY() - speed);
-                } if (this.getY() < babkaY && blockedDown == false) {
-            this.setCenterY(getCenterY() + speed);
-
-        }
-
-    }
-
-    public void die() {
-        alive = false;
-    }
-
-    public boolean isAlive() {
-        return alive;
-    }
-
-    public void checkForCollisionWall(Rectangle platform) {}
-    public void checkForCollisionBabka(Rectangle platform) {
-                babkaX = platform.getCenterX();
-                babkaY = platform.getCenterY();
-
-                Rectangle headDie = new Rectangle(this.getX() + 8, this.getY(), width - 16, 1);
-                if (headDie.intersects(platform)) {
-                    die();
-                }
-
-    }
-
-
-
-    public void setSpeed(float speed) {
-        this.speed=speed;
-    }
-
-
-}
->>>>>>> bb4a56fa2bf5f4c4b742cd85e6913c59f0cb02d4
