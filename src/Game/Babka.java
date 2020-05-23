1465b321cc553f93675d672f238895a7e646845f
Andrii Tkachenko <andrii.tkachenko@ukma.edu.ua>
2020-05-23 14:02:42 +0300

    babka full animation

--- src/Game/Babka.java
+++ src/Game/Babka.java
@@ -223,8 +223,8 @@ public class Babka extends Rectangle {
         animationSlidingRight = new Animation(new SpriteSheet(SetupGame.path+"babka_climb_left.PNG",50,50), 100);
 
 
-        animationStandLeft = new Animation(new SpriteSheet(SetupGame.path+"babka_climb_right.PNG",50,50), 100);
-        animationStandRight = new Animation(new SpriteSheet(SetupGame.path+"babka_climb_left.PNG",50,50), 100);
+        animationStandLeft = new Animation(new SpriteSheet(SetupGame.path+"babka_stand_right.PNG",50,50), 100);
+        animationStandRight = new Animation(new SpriteSheet(SetupGame.path+"babka_stand_left.PNG",50,50), 100);
 
         animationWalkingLeft = new Animation(new SpriteSheet(SetupGame.path+"babka_go_left.PNG", 50,50), 100);
         animationWalkingLeft.setPingPong(true);
